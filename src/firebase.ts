76f0937eb3c5795ae1b56e1297002f14118916c1
ManjuBodi <166508004+ManjuBodi@users.noreply.github.com>
2024-11-16 16:27:09 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBqKmPmdGcgdpK3gIGTewx2LBidJKx1EtU',
-  authDomain: 'carbon-compass-df0ef.firebaseapp.com',
-  projectId: 'carbon-compass-df0ef',
-  storageBucket: 'carbon-compass-df0ef.appspot.com',
-  messagingSenderId: '215825993019',
-  appId: '1:215825993019:web:eeb80e7c8d84858279fb1e',
-  measurementId: 'G-6FQ3T1FSQV',
+  apiKey: 'your_api_key',
+  authDomain: 'your_domain',
+  projectId: 'your_projectid',
+  storageBucket: 'your_storage_bucket',
+  messagingSenderId: 'your_id',
+  appId: 'your_app_id',
+  measurementId: 'your_measurement_id',
 };
 
 const app = initializeApp(firebaseConfig);
